be7b118d8bc089ec9eec825ac52bd32bdd619e47
Enrico Mazzon <enricomazzon@live.it>
2026-03-07 16:19:18 +0100

    Hello Window. Aggiunto glad.c.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,55 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+#include<iostream>
+
+const int SCR_WIDTH = 800;
+const int SCR_HEIGHT = 600;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void process_input(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+int main() {
+	//inizializza glfw, imposta la versione a 3.3, forza utilizzo profilo core
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cerr<< "impossibile creare finestra glfw" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	//passiamo a glad la funzione per caricare gli indirizzi delle funzioni opengl
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cerr << "impossibile inizializzare GLAD" << std::endl;
+		return -1;
+	}
+
+	//render loop
+	while (!glfwWindowShouldClose(window)) {
+		//input
+		process_input(window);
+		//comandi rendering
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		//cambio buffer
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
