571210e4565ddc5dedec98ef4331770eb1d77f4d
Enrico Mazzon <enricomazzon@live.it>
2026-03-17 21:39:52 +0100

    Spinning cube

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include<iostream>
 
 #include "Shader.h"
+#include "primitives/cube.h"
 
 constexpr int SCR_WIDTH = 800;
 constexpr int SCR_HEIGHT = 600;
@@ -46,34 +47,17 @@ int main() {
 
 	Shader shader("../src/shaders/vertex.glsl", "../src/shaders/fragment.glsl");
 
-	float vertices[] = {
-		//vertici		   //coordinate texture
-		 0.5f,  0.5f, 0.0f,   1.0f, 1.0f,   // top right
-		 0.5f, -0.5f, 0.0f,   1.0f, 0.0f,   // bottom right
-		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f,   // bottom left
-		-0.5f,  0.5f, 0.0f,   0.0f, 1.0f    // top left
-	};
-
-	unsigned int indices[] = {
-		0, 1, 3,
-		1, 2, 3
-	};
-
 	//vertex buffer objects. Dove vengono memorizzati i vertici per poi essere spediti in gpu
 	//vertex array object:dove vengono memorizzati gli attributi dei vertici
 	//element buffer object: memorizza gli indici dei triangoli
-	unsigned int VBO, VAO, EBO;
+	unsigned int VBO, VAO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(cube), cube, GL_STATIC_DRAW);
 
 	//collega il VBO con gli ingressi dello shader
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
@@ -130,20 +114,21 @@ int main() {
 	//render loop
 	//wireframe
 	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+	glEnable(GL_DEPTH_TEST);
 	while (!glfwWindowShouldClose(window)) {
 		//input
 		process_input(window);
 
 		//comandi rendering
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		//trasformo i vertici da coordinate oggetto -> coordinate mondo
 		glm::mat4 model = glm::mat4(1.0f);
 		glm::mat4 view = glm::mat4(1.0f);
 		glm::mat4 projection = glm::mat4(1.0f);
 
-		model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
 		//view transform. Muovo la scena più lontana dalla telecamera
 		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 		projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH/(float)SCR_HEIGHT, 0.1f, 100.0f);
@@ -161,8 +146,7 @@ int main() {
 		glBindTexture(GL_TEXTURE_2D, texture2);
 		shader.use();
 
-		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
 		//cambio buffer
 		glfwSwapBuffers(window);
