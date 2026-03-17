90f4acdd4130d919a44b74188f67916c0ce6a00b
Enrico Mazzon <enricomazzon@live.it>
2026-03-17 17:46:38 +0100

    Rotazione (animata) rettangolo

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include<stb_image.h>
 
 #include<glm/glm.hpp>
+#include<glm/ext.hpp>
 
 #include<iostream>
 
@@ -139,6 +140,13 @@ int main() {
 		//comandi rendering
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		auto trans = glm::mat4(1.0f);
+		trans = glm::translate(trans, glm::vec3(0.5f, -0.5f, 0.0f));
+		trans = glm::rotate(trans, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+
+		unsigned int transformLoc = glGetUniformLocation(shader.ID, "transform");
+		glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture1);
