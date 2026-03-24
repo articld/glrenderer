54cfc36192e34eb36e4bfb4be0947babe6940e87
Enrico Mazzon <enricomazzon@live.it>
2026-03-24 21:21:04 +0100

    Cambiato fragment shader per omettere posizione della camera

--- src/shaders/fragment.fs
+++ src/shaders/fragment.fs
@@ -4,14 +4,13 @@ out vec4 FragColor;
 in vec2 TexCoord;
 in vec3 Normal;
 in vec3 FragPosition;
+in vec3 LightPosition;
 
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 
 uniform vec3 objectColor;
 uniform vec3 lightColor;
-
-uniform vec3 lightPos;
 uniform vec3 viewPos;
 
 void main()
@@ -21,12 +20,12 @@ void main()
     vec3 ambient = ambientStrength * lightColor;
     //diffuse
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPosition);
+    vec3 lightDir = normalize(LightPosition - FragPosition);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
     //specular, calcolo la riflessione della luce dot posizione camera
     float specularStrength = 0.5;
-    vec3 viewDirection = normalize(viewPos - FragPosition);
+    vec3 viewDirection = normalize(- FragPosition);
     //reflect si aspetta che il primo vettore punti dalla sorgente di luce alla posizione del fragment
     vec3 reflectDirection = reflect(-lightDir, norm);
     float spec = pow(max(dot(viewDirection, reflectDirection), 0.0), 32);
